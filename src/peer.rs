d9a8e9a0276faa0d51643b9abe6cc8e4d4ab3489
Iwan BK <iwanbk@gmail.com>
2025-05-26 09:58:47 +0700

    Initial peer work code

--- src/peer.rs
+++ src/peer.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
-use futures_util::stream::{SplitSink, SplitStream};
+use futures_util::SinkExt; // Add this import for the send method
+use futures_util::stream::{SplitSink, SplitStream, StreamExt};
 use std::sync::Arc;
 use tokio::net::TcpStream;
 use tokio_tungstenite::WebSocketStream;
@@ -18,10 +19,12 @@ pub struct Peer {
     peer_id: String,
 
     /// The incoming part of the WebSocket stream
-    incoming: SplitStream<WebSocketStream<TcpStream>>,
+    rx_conn: SplitStream<WebSocketStream<TcpStream>>,
 
     /// The outgoing part of the WebSocket stream
-    outgoing: SplitSink<WebSocketStream<TcpStream>, Message>,
+    tx_conn: SplitSink<WebSocketStream<TcpStream>, Message>,
+
+    rx_chan: tokio::sync::mpsc::Receiver<Vec<u8>>,
 
     /// Reference to the store that manages peers
     store: Arc<Store>,
@@ -32,17 +35,69 @@ impl Peer {
     pub fn new(
         raw_peer_id: Vec<u8>,
         peer_id: String,
-        incoming: SplitStream<WebSocketStream<TcpStream>>,
-        outgoing: SplitSink<WebSocketStream<TcpStream>, Message>,
+        rx_conn: SplitStream<WebSocketStream<TcpStream>>,
+        tx_conn: SplitSink<WebSocketStream<TcpStream>, Message>,
+        rx_chan: tokio::sync::mpsc::Receiver<Vec<u8>>,
         store: Arc<Store>,
     ) -> Self {
         Peer {
             raw_peer_id,
             peer_id,
-            incoming,
-            outgoing,
+            rx_conn,
+            tx_conn,
+            rx_chan,
             store,
         }
+    }
+
+    pub async fn work(&mut self) {
+        // Main event loop
+        loop {
+            // Use tokio::select! to concurrently wait on multiple async operations
+            tokio::select! {
+                // Handle incoming WebSocket messages
+                message = self.rx_conn.next() => {
+                    match message {
+                        Some(Ok(msg)) => {
+                            // Process the incoming WebSocket message
+                            tracing::info!("Received WebSocket message: {:?}", msg);
+                            // TODO: Process different message types
+                        }
+                        Some(Err(e)) => {
+                            tracing::error!("Error receiving WebSocket message: {}", e);
+                            break; // Exit the loop on WebSocket error
+                        }
+                        None => {
+                            tracing::info!("WebSocket stream ended for peer {}", self.peer_id);
+                            break; // Exit the loop when WebSocket stream ends
+                        }
+                    }
+                },
+
+                // Handle messages from other peers via the channel
+                message = self.rx_chan.recv() => {
+                    match message {
+                        Some(data) => {
+                            tracing::info!("Received message via channel, size: {} bytes", data.len());
+                            // Forward messages to connected client
+                            match self.tx_conn.send(Message::Binary(data.into())).await {
+                                Ok(_) => tracing::debug!("Successfully forwarded message to peer"),
+                                Err(e) => {
+                                    tracing::error!("Failed to forward message: {}", e);
+                                    break; // Exit the loop on send error
+                                }
+                            }
+                        }
+                        None => {
+                            tracing::info!("Channel for peer {} closed", self.peer_id);
+                            break; // Exit the loop when channel is closed
+                        }
+                    }
+                }
+            }
+        }
+
+        tracing::info!("Peer {} disconnected", self.peer_id);
     }
 
     #[allow(dead_code)]
--- src/store.rs
+++ src/store.rs
@@ -1,58 +1,59 @@
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
-use crate::peer::Peer;
+use tokio::sync::mpsc::Sender;
 
 /// Store is a thread-safe store of peers
 /// It is used to store the peers that are connected to the relay server
 pub struct Store {
-    peers: RwLock<HashMap<String, Arc<Peer>>>,
+    peers_tx: RwLock<HashMap<String, Arc<Sender<Vec<u8>>>>>,
 }
 
 impl Store {
     /// Creates a new Store instance
     pub fn new() -> Self {
         Store {
-            peers: RwLock::new(HashMap::new()),
+            peers_tx: RwLock::new(HashMap::new()),
         }
     }
 
     /// Adds a peer to the store
-    pub fn add_peer(&self, peer: Arc<Peer>) {
-        let mut peers = self.peers.write().unwrap();
+    pub fn add_peer(&self, peer_id: String, peer_tx: Arc<Sender<Vec<u8>>>) {
+        let mut peers = self.peers_tx.write().unwrap();
 
         // If a peer with the same ID already exists, close it
-        if let Some(old_peer) = peers.get(&peer.to_string()) {
-            old_peer.close();
+        if let Some(old_peer) = peers.get(peer_id.as_str()) {
+            //old_peer.close();
+            tracing::info!("Closing existing peer with ID: {}", peer_id);
         }
 
-        peers.insert(peer.to_string(), peer);
+        peers.insert(peer_id, peer_tx);
     }
 
     /// Deletes a peer from the store
-    pub fn delete_peer(&self, peer: &Arc<Peer>) {
-        let mut peers = self.peers.write().unwrap();
+    pub fn delete_peer(&self, peer_id: &str, peer_tx: &Arc<Sender<Vec<u8>>>) {
+        let mut peers = self.peers_tx.write().unwrap();
 
         // Check if the peer exists in the store
-        if let Some(stored_peer) = peers.get(&peer.to_string()) {
+        if let Some(stored_peer) = peers.get(peer_id) {
             // Check if it's the same peer (by reference comparison)
-            if !Arc::ptr_eq(stored_peer, peer) {
+            if !Arc::ptr_eq(stored_peer, peer_tx) {
                 return;
             }
 
-            peers.remove(&peer.to_string());
+            peers.remove(peer_id);
         }
     }
 
     /// Returns a peer by its ID
-    pub fn get_peer(&self, id: &str) -> Option<Arc<Peer>> {
-        let peers = self.peers.read().unwrap();
+    pub fn get_peer(&self, id: &str) -> Option<Arc<Sender<Vec<u8>>>> {
+        let peers = self.peers_tx.read().unwrap();
         peers.get(id).cloned()
     }
 
     /// Returns all the peers in the store
-    pub fn get_all_peers(&self) -> Vec<Arc<Peer>> {
-        let peers = self.peers.read().unwrap();
+    pub fn get_all_peers(&self) -> Vec<Arc<Sender<Vec<u8>>>> {
+        let peers = self.peers_tx.read().unwrap();
         peers.values().cloned().collect()
     }
 }
