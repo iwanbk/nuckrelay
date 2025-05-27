c7b1bba51e7f5a4f95f562e84ec76bfe214c4107
Iwan BK <iwanbk@gmail.com>
2025-05-27 16:11:15 +0700

    Use bytes::Bytes instead of Vec<u8> for rx_chan

--- src/store.rs
+++ src/store.rs
@@ -6,7 +6,7 @@ use tokio::sync::mpsc::Sender;
 /// Store is a thread-safe store of peers
 /// It is used to store the peers that are connected to the relay server
 pub struct Store {
-    peers_tx: RwLock<HashMap<String, Arc<Sender<Vec<u8>>>>>,
+    peers_tx: RwLock<HashMap<String, Arc<Sender<bytes::Bytes>>>>,
 }
 
 impl Store {
@@ -18,11 +18,11 @@ impl Store {
     }
 
     /// Adds a peer to the store
-    pub fn add_peer(&self, peer_id: String, peer_tx: Arc<Sender<Vec<u8>>>) {
+    pub fn add_peer(&self, peer_id: String, peer_tx: Arc<Sender<bytes::Bytes>>) {
         let mut peers = self.peers_tx.write().unwrap();
 
         // If a peer with the same ID already exists, close it
-        if let Some(old_peer) = peers.get(peer_id.as_str()) {
+        if let Some(_) = peers.get(peer_id.as_str()) {
             //old_peer.close();
             tracing::info!("Closing existing peer with ID: {}", peer_id);
         }
@@ -31,7 +31,7 @@ impl Store {
     }
 
     /// Deletes a peer from the store
-    pub fn delete_peer(&self, peer_id: &str, peer_tx: &Arc<Sender<Vec<u8>>>) {
+    pub fn delete_peer(&self, peer_id: &str, peer_tx: &Arc<Sender<bytes::Bytes>>) {
         let mut peers = self.peers_tx.write().unwrap();
 
         // Check if the peer exists in the store
@@ -46,13 +46,13 @@ impl Store {
     }
 
     /// Returns a peer by its ID
-    pub fn get_peer(&self, id: &str) -> Option<Arc<Sender<Vec<u8>>>> {
+    pub fn get_peer(&self, id: &str) -> Option<Arc<Sender<bytes::Bytes>>> {
         let peers = self.peers_tx.read().unwrap();
         peers.get(id).cloned()
     }
 
     /// Returns all the peers in the store
-    pub fn get_all_peers(&self) -> Vec<Arc<Sender<Vec<u8>>>> {
+    pub fn get_all_peers(&self) -> Vec<Arc<Sender<bytes::Bytes>>> {
         let peers = self.peers_tx.read().unwrap();
         peers.values().cloned().collect()
     }
